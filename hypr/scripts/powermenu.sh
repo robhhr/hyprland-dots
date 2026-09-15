2d0eaea483c9df7bc5c67347094301cebaf58660
robhhr <robertohhr@pm.me>
2026-09-14 23:59:55 -0700

    powermenu script with specs

--- hypr/scripts/powermenu.sh
+++ hypr/scripts/powermenu.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-choice=$(echo -e "shutdown\nreboot\nsuspend\nlogout" | wofi --show dmenu --width 200 --height 140 --prompt "" --dmenu)
+choice=$(printf "SUSPEND\nLOG OUT\nRESTART\nPOWER OFF\n" | wofi --dmenu \
+    --hide-search \
+    --style ~/.config/wofi/powermenu.css \
+    --location top_right \
+    --xoffset=-24 --yoffset=12 \
+    --width 200 --height 140)
 
 case "$choice" in
-    shutdown)
+    "POWER OFF")
         systemctl poweroff
         ;;
-    reboot)
+    RESTART)
         systemctl reboot
         ;;
-    suspend)
+    SUSPEND)
         systemctl suspend
         ;;
-    logout)
+    "LOG OUT")
         hyprctl dispatch exit
         ;;
 esac
-
