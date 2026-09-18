1b7e6140c1c51725e6af43004c170d57ac001607
robhhr <robertohhr@pm.me>
2026-09-17 23:11:26 -0700

    configure powermenu module for items/dimensions

--- hypr/scripts/powermenu.sh
+++ hypr/scripts/powermenu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-choice=$(printf "SUSPEND\nLOG OUT\nRESTART\nPOWER OFF\n" | wofi --dmenu \
+items='SUSPEND\nLOG OUT\nRESTART\nPOWER OFF'
+
+height=$(( $(printf '%b\n' "$items" | wc -l) * 30 + 16 ))
+
+choice=$(printf '%b\n' "$items" | wofi --dmenu \
     --hide-search \
     --style ~/.config/wofi/powermenu.css \
     --location top_right \
-    --xoffset=-24 --yoffset=12 \
-    --width 200 --height 140)
+    --xoffset=-32 --yoffset=12 \
+    --width 200 --height "$height")
 
 case "$choice" in
     "POWER OFF")
